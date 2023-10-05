8e4c5869be70551b538265cc047fe7476aa921bb
calisse <lapodispoto@gmail.com>
2023-10-05 17:33:05 +0200

    Added env.sh

---
+++ samplerep/utils.py
@@ -0,0 +1,32 @@
+
+from typing import Union
+
+import numpy as np
+
+
+def square(value : Union[int, float, np.ndarray]]) -> Union[float, np.ndarray]:
+"""Return the square of a given number
+   This will accept any input that support the ** operator.
+   
+   Function will raise a TypeError for incompatible types such as strings
+   
+   Arguments
+   ---------
+   value: number, ndarray
+   	The input value to square, each element in the ndarray is squared,
+   	
+   	
+   Return
+   ------
+   	The square of the input value : float or ndarray.
+	
+	
+	
+	
+"""
+
+	return x**2
+	
+	
+
+
